75d3a103afb88385e6dd0d87ce094ff9ab7c1abd
Rizwan Choudhary <Rizwan Choudhary>
2017-04-28 18:08:05 -0400

    new services panel and ditching domain service

--- front-page.php
+++ front-page.php
@@ -4,13 +4,40 @@
 ?>
 <div class="home">
     <div class="hero-content-riz">
-        <div class="domain-check-component">
-            <h1>Enter your website to see if it is available</h1>
+        <div class="a-frame">
+            <h1>Every Business needs a website</h1>
             <label for="website"></label>
             <input id="website" name="website"/>
             <div class="home-button-riz">
                 <button onclick="checkdomain()" class="btn">Submit</button>
             </div>
+        </div>
+    </div>
+    <div class="services-panel-riz">
+        <div class="services-panel-riz--inner aligncenter">
+            <ul class="service-items">
+                <li>
+                    <h3>Information</h3>
+                    <img src="http://placehold.it/150x150"/>
+                    <div class="service-info-riz">
+                        lipsum blah blah
+                    </div>
+                </li>
+                            <li>
+                    <h3>Information</h3>
+                    <img src="http://placehold.it/150x150"/>
+                    <div class="service-info-riz">
+                        lipsum blah blah
+                    </div>
+                </li>
+                            <li>
+                    <h3>Information</h3>
+                    <img src="http://placehold.it/150x150"/>
+                    <div class="service-info-riz">
+                        lipsum blah blah
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
